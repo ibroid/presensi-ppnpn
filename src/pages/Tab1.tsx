73512d081137bd318cf7a1e8332c4949aca618a8
ibroid <maulana858@gmail.com>
2023-02-19 13:43:44 +0700

    update menu

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,74 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonSpinner, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
+
 import './Tab1.css';
+import { Geolocation } from '@capacitor/geolocation';
+import { useState } from 'react';
+import '../components/ExploreContainer.css';
+import { GoogleMap } from '@capacitor/google-maps';
+import { useRef } from 'react';
 
 const Tab1: React.FC = () => {
+  const mapRef = useRef<HTMLElement>();
+  const [locationNow, setLocationNow] = useState<string>("Lokasi Belum Ditentukan");
+  let newMap: GoogleMap;
+
+  async function createMap() {
+    if (!mapRef.current) return;
+
+    newMap = await GoogleMap.create({
+      id: 'my-cool-map',
+      element: mapRef.current,
+      apiKey: "",
+      config: {
+        center: {
+          lat: 33.6,
+          lng: -117.9
+        },
+        zoom: 8
+      }
+    })
+  }
+
+
+  const getLocation = async () => {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current position:', coordinates);
+    setLocationNow("Pengadilan Agama Jakarta Utara")
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle size="large">Presensi Kehadiran</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Presensi Kehadiran</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div className="container">
+          <capacitor-google-map ref={mapRef} style={{
+            display: 'inline-block',
+            width: 275,
+            height: 400
+          }}></capacitor-google-map>
+          <button onClick={createMap}>Create Map</button>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12" ><strong>{locationNow}</strong></IonCol>
+              <IonCol size="12" ><IonCol><p>{locationNow}</p></IonCol></IonCol>
+              <IonCol size="12" ><IonSpinner /></IonCol>
+              <IonCol size="12" >
+                <IonButton onClick={() => getLocation()}>
+                  Dapatkan Lokasi
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,66 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonDatetime, IonGrid, IonRow, IonItem, IonAvatar, IonLabel, IonCol } from '@ionic/react';
+import { Virtuoso } from 'react-virtuoso';
+import '../components/ExploreContainer.css'
 import './Tab2.css';
+import '../global.css';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Riwayat Presensi</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Riwayat Presensi</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonGrid>
+          <IonRow class="ion-margin">
+            <IonDatetime color={"primary"} showDefaultTimeLabel={false}></IonDatetime>
+          </IonRow>
+          <IonRow class="ion-margin">
+            <h4>Daftar Presensi Tanggal Sekian</h4>
+          </IonRow>
+        </IonGrid>
+        <Virtuoso
+          style={{ height: '100%' }}
+          data={[
+            'Imal',
+            'Adi',
+            'Rudi',
+            'Faiz',
+            'Safrudi',
+            'Dela',
+            'Iwan',
+            'Aulia',
+            'Yanti',
+            'Aldi',
+            'Samsuri',
+          ]}
+          itemContent={(index, data) => {
+            return (
+              <div style={{ height: '56px' }}>
+                <IonItem>
+                  <IonAvatar slot="start">
+                    <img src="https://picsum.photos/seed/picsum/40/40" />
+                  </IonAvatar>
+                  <IonLabel>
+                    <IonGrid>
+                      <IonRow>
+                        <IonCol>{data}</IonCol>
+                        <IonCol size='2'>07:00</IonCol>
+                      </IonRow>
+                    </IonGrid>
+                  </IonLabel>
+                </IonItem>
+              </div>
+            );
+          }}
+        />
       </IonContent>
     </IonPage>
   );
